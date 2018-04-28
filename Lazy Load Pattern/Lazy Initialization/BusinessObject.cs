2cb24e03e39df7b5739af0c5f90c0a894508007d
pham-huyson87 <pham.huyson87@hotmail.com>
2018-04-28 10:28:13 -0400

    Lazy Load Pattern - Lazy Initialization
    Lazy Load Pattern - Lazy Initialization

---
+++ Lazy Load Pattern/Lazy Initialization/BusinessObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lazy_Load_Pattern.Lazy_Initialization
+{
+    class BusinessObject
+    {
+        // Without Lazy<T>
+
+        private object _property;
+        public object Property
+        {
+            get
+            {
+                if (_property == null) {
+                    _property = new object();
+                }
+
+                return _property;
+            }
+        }
+
+
+        // With Lazy<T>
+
+        private Lazy<object> _propertyV2;
+        public object PropertyV2
+        {
+            get
+            {
+                return _propertyV2.Value;
+            }
+        }
+
+        public BusinessObject()
+        {
+            _propertyV2 = new Lazy<object>(() => new object());  // Thread-safe by default
+        }
+    }
+}
---
+++ Lazy Load Pattern/Lazy Initialization/Client.cs
@@ -0,0 +1,14 @@
+﻿namespace Lazy_Load_Pattern.Lazy_Initialization
+{
+    class Client
+    {
+        public Client()
+        {
+            BusinessObject businessObject = new BusinessObject();
+
+            var value = businessObject.Property;
+
+            value = businessObject.PropertyV2;
+        }
+    }
+}
